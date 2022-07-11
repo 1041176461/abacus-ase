f26914d06c36b8e58075f9211fe50a344788874b
jiyuang <jiyuyang@mail.ustc.com>
2022-07-11 15:09:40 +0800

    delete some files

--- files/abacus/__init__.py
+++ files/abacus/__init__.py
@@ -5,7 +5,7 @@ Created on Mon Apr 23 14:30:24 2018
 """
 
 """
-from abacus_out import Abacus
-from create_input import AbacusInput
+from .abacus import Abacus
+from .create_input import AbacusInput
 __all__ = ['Abacus', 'AbacusInput']
 """
